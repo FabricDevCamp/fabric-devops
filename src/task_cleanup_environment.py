9271fb90f604edacf90479102f55c58e8dee726a
Ted Pattison <ted@tedpattison.net>
2025-05-09 15:53:32 -0400

    updates

--- src/task_cleanup_environment.py
+++ src/task_cleanup_environment.py
@@ -1,5 +1,11 @@
 """Cleanup Environment"""
 
-from fabric_devops import DeploymentManager
+from fabric_devops import DeploymentManager, GitHubRestApi, AppLogger
+
+AppLogger.log_step('Deleting Demo Repos')
+repos = GitHubRestApi.get_github_repositories()
+for repo in repos:
+    AppLogger.log_substep(f"Deleting {repo['name']} [{repo['id']}]")
+    GitHubRestApi.delete_github_repository(repo['name'])
 
 DeploymentManager.cleanup_dev_environment()
