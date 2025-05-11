4d207fcc01aa367a21872b912f8c2c00263347a1
Ted Pattison <ted@tedpattison.net>
2025-05-11 10:45:22 -0400

    Updates

--- src/setup_workspace_with_git_connection.py
+++ src/setup_workspace_with_git_connection.py
@@ -2,26 +2,33 @@
 
 import os
 
-from fabric_devops import DeploymentManager
+from fabric_devops import DeploymentManager, AppSettings, FabricRestApi
 
 WORKSPACE = None
 
 match os.getenv("SOLUTION_NAME"):
 
     case 'Custom Power BI Solution':
-        WORKSPACE = DeploymentManager.deploy_powerbi_solution('Custom Power BI Solution')
+        WORKSPACE = DeploymentManager.deploy_powerbi_solution(
+            'Custom Power BI Solution')
 
     case 'Custom Notebook Solution':
-        WORKSPACE = DeploymentManager.deploy_notebook_solution('Custom Notebook Solution')
+        WORKSPACE = DeploymentManager.deploy_notebook_solution(
+            'Custom Notebook Solution')
 
     case 'Custom Shortcut Solution':
-        WORKSPACE = DeploymentManager.deploy_shortcut_solution('Custom Shortcut Solution')
+        WORKSPACE = DeploymentManager.deploy_shortcut_solution(
+            'Custom Shortcut Solution')
 
     case 'Custom Data Pipeline Solution':
-        WORKSPACE = DeploymentManager.deploy_data_pipeline_solution('Custom Data Pipeline Solution')
+        WORKSPACE = DeploymentManager.deploy_data_pipeline_solution(
+            'Custom Data Pipeline Solution')
 
     case 'Custom Variable Library Solution':
-        WORKSPACE = DeploymentManager.deploy_variable_library_solution('Custom Variable Library Solution')
+        # SPN bug with var lib requires user auth
+        AppSettings.RUN_AS_SERVICE_PRINCIPAL = False
+        FabricRestApi.authenticate()
+        WORKSPACE = DeploymentManager.deploy_variable_library_solution(
+            'Custom Variable Library Solution')
 
 DeploymentManager.connect_workspace_to_github_repo(WORKSPACE)
-
