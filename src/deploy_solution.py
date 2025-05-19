b6b7f3b49ac9618eae7ffc4cf15270c65759da49
Ted Pattison <ted@tedpattison.net>
2025-05-19 13:42:47 -0400

    updates

--- src/deploy_solution.py
+++ src/deploy_solution.py
@@ -335,39 +335,46 @@ def deploy_variable_library_solution():
 def deploy_warehouse_solution():
     """Deploy Warehouse Solution"""
 
-    WORKSPACE_NAME = "Custom Warehouse Solution"
-    LAKEHOUSE_NAME = "staging"
-    WAREHOUSE_NAME = "sales"
+    workspace_name = "Custom Warehouse Solution"
+    lakehouse_name = "staging"
+    warehouse_name = "sales"
 
-    DATA_PIPELINES = [
-        { 'name': 'Load Tables in Staging Lakehouse', 'template':'LoadTablesInStagingLakehouse.json'},
-        { 'name': 'Create Warehouse Tables', 'template':'CreateWarehouseTables.json'},
-        { 'name': 'Create Warehouse Stored Procedures', 'template':'CreateWarehouseStoredProcedures.json'},
-        { 'name': 'Refresh Warehouse Tables', 'template':'RefreshWarehouseTables.json'}
+    data_pipelines = [
+        { 'name': 'Load Tables in Staging Lakehouse',
+          'template':'LoadTablesInStagingLakehouse.json'},
+        { 'name': 'Create Warehouse Tables',
+          'template':'CreateWarehouseTables.json'},
+        { 'name': 'Create Warehouse Stored Procedures',
+          'template':'CreateWarehouseStoredProcedures.json'},
+        { 'name': 'Refresh Warehouse Tables',
+          'template':'RefreshWarehouseTables.json'}
     ]
 
-    SEMANTIC_MODEL_NAME = 'Product Sales DirectLake Model'
-    REPORTS = [
-        { 'name': 'Product Sales Summary', 'template':'product_sales_summary.json'},
-        { 'name': 'Product Sales Time Intelligence', 'template':'product_sales_time_intelligence.json'},
-        { 'name': 'Product Sales Top 10 Cities', 'template':'product_sales_top_ten_cities_report.json'},
+    semantic_model_name = 'Product Sales DirectLake Model'
+    reports  = [
+        { 'name': 'Product Sales Summary',
+          'template':'product_sales_summary.json'},
+        { 'name': 'Product Sales Time Intelligence',
+          'template':'product_sales_time_intelligence.json'},
+        { 'name': 'Product Sales Top 10 Cities',
+          'template':'product_sales_top_ten_cities_report.json'},
     ]
 
     AppLogger.log_job("Deploying Warehouse solution")
 
-    workspace = FabricRestApi.create_workspace(WORKSPACE_NAME)
+    workspace = FabricRestApi.create_workspace(workspace_name)
 
     data_prep_folder = FabricRestApi.create_folder(workspace['id'], 'data_prep')
     data_prep_folder_id = data_prep_folder['id']
 
     lakehouse = FabricRestApi.create_lakehouse(
         workspace['id'],
-        LAKEHOUSE_NAME,
+        lakehouse_name,
         data_prep_folder_id)
 
     warehouse = FabricRestApi.create_warehouse(
         workspace['id'],
-        WAREHOUSE_NAME
+        warehouse_name
     )
 
     warehouse_connect_string = FabricRestApi.get_warehouse_connection_string(
@@ -379,48 +386,132 @@ def deploy_warehouse_solution():
         AppSettings.AZURE_STORAGE_PATH,
         workspace)
 
-    for data_pipeline in DATA_PIPELINES:
+    for data_pipeline in data_pipelines:
         template_file = f"DataPipelines//{data_pipeline['template']}"
         template_content = ItemDefinitionFactory.get_template_file(template_file)
-        template_content = template_content.replace('{WORKSPACE_ID}', workspace['id']) \
-                                        .replace('{LAKEHOUSE_ID}', lakehouse['id']) \
-                                        .replace('{WAREHOUSE_ID}', warehouse['id']) \
-                                        .replace('{WAREHOUSE_CONNECT_STRING}', warehouse_connect_string) \
-                                        .replace('{CONNECTION_ID}', connection['id'])
-        
+        template_content = \
+            template_content.replace('{WORKSPACE_ID}', workspace['id']) \
+                            .replace('{LAKEHOUSE_ID}', lakehouse['id']) \
+                            .replace('{WAREHOUSE_ID}', warehouse['id']) \
+                            .replace('{WAREHOUSE_CONNECT_STRING}', warehouse_connect_string) \
+                            .replace('{CONNECTION_ID}', connection['id'])
 
         pipeline_create_request = ItemDefinitionFactory.get_data_pipeline_create_request(
             data_pipeline['name'],
             template_content)
-        
+
         pipeline = FabricRestApi.create_item(
-            workspace['id'], 
+            workspace['id'],
             pipeline_create_request,
             data_prep_folder_id)
 
         FabricRestApi.run_data_pipeline(workspace['id'], pipeline)
 
 
-    createModelRequest = \
-        ItemDefinitionFactory.get_directlake_model_create_request(SEMANTIC_MODEL_NAME,
-                                                                'sales_model_DirectLake.bim',
-                                                                warehouse_connect_string,
-                                                                warehouse['id'])
+    create_model_request = \
+        ItemDefinitionFactory.get_directlake_model_create_request(
+            semantic_model_name,
+            'sales_model_DirectLake.bim',
+            warehouse_connect_string,
+            warehouse['id'])
 
-    model = FabricRestApi.create_item(workspace['id'], createModelRequest)
+    model = FabricRestApi.create_item(workspace['id'], create_model_request)
 
     FabricRestApi.create_and_bind_semantic_model_connecton(workspace, model['id'], lakehouse)
 
-    for report_data in REPORTS:
-        create_report_request = ItemDefinitionFactory.get_report_create_request(model['id'],
-                                                                                report_data['name'],
-                                                                                report_data['template'])
-        report = FabricRestApi.create_item(workspace['id'], create_report_request)
+    for report_data in reports:
+
+        create_report_request = ItemDefinitionFactory.get_report_create_request(
+            model['id'],
+            report_data['name'],
+            report_data['template'])
+
+        FabricRestApi.create_item(workspace['id'], create_report_request)
+
+    AppLogger.log_job_ended("Solution deployment complete")
+
+def deploy_realtime_solution():
+    """Deploy Real Time Solution"""
+
+    AppLogger.log_job("Deploying Real Time Solution")
+
+    workspace_name = "Custom Realtime Solution"
+    eventhouse_name = "Eventhouse01"
+    kql_database_name = "KqlDb01"
+    eventstream_name = "Eventstream01"
+    realtime_dashboard_name = "RealtimeDashboard01"
+    semantic_model_name = 'Product Sales Imported Model'
+    report_name = 'Product Sales Summary'
+
+    workspace = FabricRestApi.create_workspace(workspace_name)
+
+    create_eventhouse_request = \
+        ItemDefinitionFactory.get_eventhouse_create_request(eventhouse_name)
+    
+    eventhouse_item = FabricRestApi.create_item(workspace['id'], create_eventhouse_request)
+
+    eventhouse = FabricRestApi.get_eventhouse(workspace['id'], eventhouse_item['id'])
+
+    query_service_uri = eventhouse['properties']['queryServiceUri']
+    ingestion_service_uri  = eventhouse['properties']['ingestionServiceUri']
+
+
+    create_kql_database_request = \
+        ItemDefinitionFactory.get_kql_database_create_request(kql_database_name, 
+                                                              eventhouse)
+    
+    kql_database = FabricRestApi.create_item(workspace['id'], create_kql_database_request)
+
+    create_eventstream_request = \
+        ItemDefinitionFactory.get_eventstream_create_request(eventstream_name,
+                                                             workspace['id'],
+                                                             eventhouse['id'],
+                                                             kql_database)
+    
+    eventstream = FabricRestApi.create_item(workspace['id'], create_eventstream_request)
+
+    realtime_dashboard_create_request = ItemDefinitionFactory.get_kql_dashboard_create_request(
+        realtime_dashboard_name,
+        workspace['id'],
+        kql_database,
+        query_service_uri)
+    
+    realtime_dashboard = FabricRestApi.create_item(workspace['id'], realtime_dashboard_create_request)
+
+    create_queryset1_create_request = ItemDefinitionFactory.get_kql_queryset_create_request(
+        'Queryset1', kql_database, query_service_uri, 'RealTimeQueryset1.json'
+    )
+
+    queryset1 = FabricRestApi.create_item(workspace['id'], create_queryset1_create_request)
+
+    create_queryset2_create_request = ItemDefinitionFactory.get_kql_queryset_create_request(
+        'Queryset2', kql_database, query_service_uri, 'RealTimeQueryset2.json'
+    )
+
+    queryset2 = FabricRestApi.create_item(workspace['id'], create_queryset2_create_request)
+
+
+
+    # web_url = FabricRestApi.get_web_url_from_semantic_model(workspace['id'], model['id'])
+
+    # AppLogger.log_substep(f'Creating anonymous Web connection to {web_url} ')
+
+    # connection = FabricRestApi.create_anonymous_web_connection(web_url, workspace)
+
+    # FabricRestApi.bind_semantic_model_to_connection(workspace['id'], model['id'], connection['id'])
+
+    # FabricRestApi.refresh_semantic_model(workspace['id'], model['id'])
+
+    # create_report_request = \
+    #     ItemDefinitionFactory.get_report_create_request(model['id'],
+    #                                                     report_name,
+    #                                                     'product_sales_summary.json')
+
+    # FabricRestApi.create_item(workspace['id'], create_report_request)
 
     AppLogger.log_job_ended("Solution deployment complete")
 
 
-## end
 match os.getenv("SOLUTION_NAME"):
 
     case 'Custom Power BI Solution':
@@ -440,6 +531,9 @@ match os.getenv("SOLUTION_NAME"):
 
     case 'Custom Warehouse Solution':
         deploy_warehouse_solution()
+
+    case 'Custom Realtime Inteligence Solution':
+        deploy_realtime_solution()
 
     case 'Deploy All Solutions':
         deploy_powerbi_solution()
