acb66399e6c6e6024be9638f95a94557e0f0bf9f
Ted Pattison <ted@tedpattison.net>
2025-05-22 08:43:49 -0400

    updates

--- src/test.py
+++ src/test.py
@@ -1,20 +1,5 @@
-"""Deploy Power BI Solution"""
+"""Test"""
 
-import os
+from fabric_devops import DeploymentManager, FabricRestApi
 
-_token_cache_folder = './/Cash//'
-_token_cache_file = 'token-cache.bin'
-_token_cache = None
-
-
-if not os.path.exists(_token_cache_folder):
-        os.makedirs(_token_cache_folder)
-
-cache_file_path = os.path.join(
-    _token_cache_folder,
-    _token_cache_file)
-
-cache_file = open(cache_file_path, 'w', encoding='utf-8')
-cache_file.write('hey baby')
-
-# cls._token_cache.serialize()
+FabricRestApi.display_workspaces()
