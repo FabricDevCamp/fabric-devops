9a62dc00b01ae7e7e65400bca2b208f5a493021e
Ted Pattison <ted@tedpattison.net>
2025-06-16 18:07:14 -0400

    FabCon solution

--- src/test.py
+++ src/test.py
@@ -1,21 +1,58 @@
 "Test"""
 
+from fabric_devops import StagingEnvironments, FabricRestApi, \
+                          AppLogger, DeploymentJob
+   
+def deploy_fabcon_solution(
+        target_workspace,
+        deploy_job = StagingEnvironments.get_dev_environment()):
+    """Deploy FabCon Solution"""
 
-from fabric_devops import DeploymentManager, StagingEnvironments
+    bronze_lakehouse_name = "sales_bronze"
+    silver_lakehouse_name = "sales_silver"
+    gold_warehouse_name = "sales"
 
-PROJECT_NAME = "Adonis"
+    # semantic_model_folder = 'Product Sales DirectLake Model.SemanticModel'
+    # report_folders = [
+    #     'Product Sales Summary.Report'
+    # ]
 
-DEPLOYMENT_PIPELINE_NAME = PROJECT_NAME
-DEV_WORKSPACE_NAME = f"{PROJECT_NAME}-dev"
-TEST_WORKSPACE_NAME = f"{PROJECT_NAME}-test"
-PROD_WORKSPACE_NAME = f"{PROJECT_NAME}"
 
-DeploymentManager.apply_post_deploy_fixes(
-    TEST_WORKSPACE_NAME,
-    StagingEnvironments.get_test_environment())
+    AppLogger.log_job(f"Deploying FabCon Solution to [{target_workspace}]")
 
-# DeploymentManager.deploy_from_test_to_prod(PROJECT_NAME)
+    deploy_job.display_deployment_parameters()
 
-# DeploymentManager.apply_post_deploy_fixes(
-#     PROD_WORKSPACE_NAME,
-#     StagingEnvironments.get_prod_environment())
+    workspace = FabricRestApi.create_workspace(target_workspace)
+
+    FabricRestApi.update_workspace_description(workspace['id'], 'Custom FabCon Solution')
+
+    bronze_lakehouse = FabricRestApi.create_lakehouse(workspace['id'], bronze_lakehouse_name)
+
+    adls_container_name = deploy_job.parameters[DeploymentJob.adls_container_name_parameter]
+    adls_container_path = deploy_job.parameters[DeploymentJob.adls_container_path_parameter]
+    adls_server = deploy_job.parameters[DeploymentJob.adls_server_parameter]
+    adls_path = f'/{adls_container_name}{adls_container_path}'
+
+    connection = FabricRestApi.create_azure_storage_connection_with_sas_token(
+        adls_server,
+        adls_path,
+        workspace)
+
+    shortcut_name = "sales-data"
+    shortcut_path = "Files"
+    shortcut_location = adls_server
+    shortcut_subpath = adls_path
+
+    FabricRestApi.create_adls_gen2_shortcut(workspace['id'],
+                                            bronze_lakehouse['id'],
+                                            shortcut_name,
+                                            shortcut_path,
+                                            shortcut_location,
+                                            shortcut_subpath,
+                                            connection['id'])
+    
+    AppLogger.log_job_complete(workspace['id'])
+
+    return workspace
+
+deploy_fabcon_solution('Contoso')
