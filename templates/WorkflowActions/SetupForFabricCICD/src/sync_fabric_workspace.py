e73a643a4b569628ab1f5b0d873e5cae541eac59
Ted Pattison <ted@tedpattison.net>
2025-09-15 14:08:27 -0400

    updates

--- templates/WorkflowActions/SetupForFabricCICD/src/sync_fabric_workspace.py
+++ templates/WorkflowActions/SetupForFabricCICD/src/sync_fabric_workspace.py
@@ -1,51 +1,76 @@
 
-import argparse
+import json
 import os
 
-from azure.identity import AzureCliCredential
+from azure.identity import AzureCliCredential, ClientSecretCredential
 
 from fabric_cicd import FabricWorkspace
 
-# Establish parser for command line arguments
-parser = argparse.ArgumentParser(description="Input Variables")
-parser.add_argument("--workspace_name", type=str, required=True)
-args = parser.parse_args()
+client_id = os.getenv("FABRIC_CLIENT_ID")
+client_secret = os.getenv("FABRIC_CLIENT_SECRET")
+tenant_id = os.getenv("FABRIC_TENANT_ID")
+token_credential = \
+    ClientSecretCredential(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id)
 
-# Initialize the FabricWorkspace object with the required parameters
-# fabric-cicd is being used to simplify authentication to the APIs
-target_workspace = FabricWorkspace(
-    workspace_name=args.workspace_name,
-    repository_directory=".",  # required, but not applicable
-    item_type_in_scope=["Notebook"], # required, but not applicable
-    token_credential=AzureCliCredential(),
-)
+github_workpace = os.getenv('GITHUB_WORKSPACE')
+branch = os.getenv('BRANCH_NAME')
+print(github_workpace, flush=True)
 
-# Update Git credentials in Fabric
-# https://learn.microsoft.com/en-us/rest/api/fabric/core/git/update-my-git-credentials
-git_credential_url = f"{target_workspace.base_api_url}/git/myGitCredentials"
-git_credential_body = {
-    "source": "ConfiguredConnection",
-    "connectionId": "47d1f273-7091-47c4-b45d-df8f1231ea74",
-}
-target_workspace.endpoint.invoke(method="PATCH", url=git_credential_url, body=git_credential_body)
+config_file = github_workpace +  '/workspace/workspace.config.json'
 
-# Get commit heads from workspace and remote branch
-# https://learn.microsoft.com/en-us/rest/api/fabric/core/git/get-status
-git_status_url = f"{target_workspace.base_api_url}/git/status"
-git_status = target_workspace.endpoint.invoke(method="GET", url=git_status_url)
-workspace_head = git_status["body"]["workspaceHead"]
-remote_head = os.getenv("BUILD_SOURCEVERSION")
+if os.path.exists(config_file) is False:
+    print(f"'{config_file}' does not exists.")
+else:        
+    print(config_file, flush=True)
 
-# Force update to sync workspace with remote branch
-# https://learn.microsoft.com/en-us/rest/api/fabric/core/git/update-from-git
-git_update_url = f"{target_workspace.base_api_url}/git/updateFromGit"
-git_update_body = {
-    "workspaceHead": f"{workspace_head}",
-    "remoteCommitHash": f"{remote_head}",
-    "conflictResolution": {
-        "conflictResolutionType": "Workspace",
-        "conflictResolutionPolicy": "PreferRemote",
-    },
-    "options": {"allowOverrideItems": True},
-}
-target_workspace.endpoint.invoke(method="POST", url=git_update_url, body=git_update_body, max_retries=10)
+    with open(config_file, 'r', encoding='utf-8') as file:
+        config = json.load(file)
+        print(config, flush=True)
+
+    workspace_config = config[branch] 
+
+    # Sample values for FabricWorkspace parameters
+    workspace_id = workspace_config['workspace_id']
+    environment = workspace_config['environment']
+    
+    repository_directory = "workspace"
+    item_type_in_scope = [ "Lakehouse", "Notebook", "SemanticModel", "Report"]
+
+    # Initialize the FabricWorkspace object with the required parameters
+    target_workspace = FabricWorkspace(
+        workspace_id=workspace_id,
+        environment=environment,
+        repository_directory=repository_directory,
+        item_type_in_scope=item_type_in_scope,
+        token_credential=token_credential,
+    )
+
+    # Update Git credentials in Fabric
+    # https://learn.microsoft.com/en-us/rest/api/fabric/core/git/update-my-git-credentials
+    git_credential_url = f"{target_workspace.base_api_url}/git/myGitCredentials"
+    git_credential_body = {
+        "source": "ConfiguredConnection",
+        "connectionId": "47d1f273-7091-47c4-b45d-df8f1231ea74",
+    }
+    target_workspace.endpoint.invoke(method="PATCH", url=git_credential_url, body=git_credential_body)
+
+    # Get commit heads from workspace and remote branch
+    # https://learn.microsoft.com/en-us/rest/api/fabric/core/git/get-status
+    git_status_url = f"{target_workspace.base_api_url}/git/status"
+    git_status = target_workspace.endpoint.invoke(method="GET", url=git_status_url)
+    workspace_head = git_status["body"]["workspaceHead"]
+    remote_head = os.getenv("BUILD_SOURCEVERSION")
+
+    # Force update to sync workspace with remote branch
+    # https://learn.microsoft.com/en-us/rest/api/fabric/core/git/update-from-git
+    git_update_url = f"{target_workspace.base_api_url}/git/updateFromGit"
+    git_update_body = {
+        "workspaceHead": f"{workspace_head}",
+        "remoteCommitHash": f"{remote_head}",
+        "conflictResolution": {
+            "conflictResolutionType": "Workspace",
+            "conflictResolutionPolicy": "PreferRemote",
+        },
+        "options": {"allowOverrideItems": True},
+    }
+    target_workspace.endpoint.invoke(method="POST", url=git_update_url, body=git_update_body, max_retries=10)
