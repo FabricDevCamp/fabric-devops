3c148603f74c340689534ebe4e62c9a6175dd24d
Ted Pattison <ted@tedpattison.net>
2025-09-17 09:59:31 -0400

    updates

--- src/deploy_with_fabric_cicd_to_ado.py
+++ src/deploy_with_fabric_cicd_to_ado.py
@@ -83,3 +83,16 @@ AdoProjectManager.write_file_to_repo(
 
 AdoProjectManager.create_and_merge_pull_request(PROJECT_NAME, 'dev','test')
 AdoProjectManager.create_and_merge_pull_request(PROJECT_NAME, 'test','main')
+
+# create first feature workspace
+FEATURE_NAME = 'feature1'
+FEATURE_WORKSPACE_NAME = F'{PROJECT_NAME}-dev-{FEATURE_NAME}'
+FEATURE_WORKSPACE = FabricRestApi.create_workspace(FEATURE_WORKSPACE_NAME)
+
+AdoProjectManager.create_branch(PROJECT_NAME, FEATURE_NAME, "dev")
+FabricRestApi.connect_workspace_to_ado_repo(FEATURE_WORKSPACE, PROJECT_NAME, FEATURE_NAME)
+
+DeploymentManager.apply_post_deploy_fixes(
+    FEATURE_WORKSPACE_NAME,
+    StagingEnvironments.get_dev_environment(),
+    True)
