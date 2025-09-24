d92cbeed8831e7546dea86323562377b586322e2
Ted Pattison <ted@tedpattison.net>
2025-09-24 17:08:37 -0400

    updates

--- src/Test1.py
+++ src/Test1.py
@@ -1,28 +1,29 @@
 """Test1"""
-import json
-from fabric_devops import DeploymentManager, AppLogger, FabricRestApi
 
+from fabric_devops import DeploymentManager, StagingEnvironments, AppLogger, FabricRestApi
 
-WORKSPACE_NAME = "Puff"
+SOLUTION_NAME = "Custom Power BI Solution"
+PROJECT_NAME = "Pammy"
 
-workspace = DeploymentManager.deploy_udf_solution(WORKSPACE_NAME)
+DEPLOYMENT_PIPELINE_NAME = PROJECT_NAME
+DEV_WORKSPACE_NAME = f"{PROJECT_NAME}-dev"
+TEST_WORKSPACE_NAME = f"{PROJECT_NAME}-test"
+PROD_WORKSPACE_NAME = f"{PROJECT_NAME}"
 
-AppLogger.log_job_complete( workspace['id'] )
+DeploymentManager.delete_deployment_pipeline_by_name(PROJECT_NAME)
 
+DeploymentManager.setup_deployment_pipeline(PROJECT_NAME, SOLUTION_NAME)
 
+DeploymentManager.deploy_from_dev_to_test(PROJECT_NAME)
 
+DeploymentManager.apply_post_deploy_fixes(
+    TEST_WORKSPACE_NAME,
+    StagingEnvironments.get_test_environment(),
+    run_etl_jobs=True)
 
+DeploymentManager.deploy_from_test_to_prod(PROJECT_NAME)
 
-# udf = FabricRestApi.get_item_by_name(workspace['id'], 'hello_fabric', 'UserDataFunction')
-
-# response = FabricRestApi.call_user_defined_function(
-#     workspace['id'],
-#     udf['id'],
-#     'hello_fabric',
-#     {
-#         "name": "TeddyP"
-#     }
-# )
-
-# AppLogger.log_job_complete( json.dumps(response) )
-
+DeploymentManager.apply_post_deploy_fixes(
+    PROD_WORKSPACE_NAME,
+    StagingEnvironments.get_prod_environment(),
+    run_etl_jobs=True)
