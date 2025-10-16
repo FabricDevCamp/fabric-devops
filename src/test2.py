dfc25889f19bddbdac61fd411967dc2eacb69916
Ted Pattison <ted@tedpattison.net>
2025-10-16 16:25:27 -0400

    updates

--- src/test2.py
+++ src/test2.py
@@ -1,78 +1,20 @@
 import json
+import os
 from fabric_devops import DeploymentManager, EnvironmentSettings, FabricRestApi
 
-EnvironmentSettings.RUN_AS_SERVICE_PRINCIPAL = False
-
-WORKSPACE_NAME = "NFL Agent"
-
-lakehouse_name = "nfl_data"
-
-model_name = "NFL Stats"
-
-workspace = FabricRestApi.get_workspace_by_name(WORKSPACE_NAME)
-
-lakehouse = FabricRestApi.get_item_by_name(workspace['id'], lakehouse_name, 'Lakehouse')
-onelake_path = FabricRestApi.get_onelake_path_for_lakehouse(workspace['id'], lakehouse)
-
-
-model = FabricRestApi.get_item_by_name(workspace['id'], model_name, 'SemanticModel')
-sources = FabricRestApi.list_datasources_for_semantic_model(workspace['id'], model['id'])
-
-print( json.dumps(sources, indent=4) )
 
 
 
-# agent = FabricRestApi.create_item(workspace['id'], create_agent_request)
+params = DeploymentManager.generate_parameter_yml_file(
+    "Charo-dev",
+    "Charo-test",
+    "Charo"
+)
 
-# create_model_request = \
-#     ItemDefinitionFactory.get_create_item_request_from_folder(
-#         'NFL Stats.SemanticModel')
+folder_path = ".//exports//params/"
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
 
-# model_redirects = {
-#     '{WORKSPACE_ID}': workspace['id'],
-#     '{LAKRHOUSE_ID}': lakehouse['id']
-# }
-
-# create_model_request = \
-#     ItemDefinitionFactory.update_part_in_create_request(
-#         create_model_request,
-#         'definition/expressions.tmdl',
-#         model_redirects)
-
-# model = FabricRestApi.create_item(workspace['id'], create_model_request)
-
-# FabricRestApi.create_and_bind_semantic_model_connecton(workspace, model['id'], lakehouse)
-
-# report_folder  = 'NFL Stats.Report'
-
-# create_report_request = \
-#     ItemDefinitionFactory.get_create_report_request_from_folder(
-#         report_folder,
-#         model['id'])
-
-# FabricRestApi.create_item(workspace['id'], create_report_request)
-
-#     # create_agent_request = \
-# #     ItemDefinitionFactory.get_create_item_request_from_folder(
-# #         'Sales Agent.DataAgent')
-
-# # agent_redirects = {
-# #     # '{WORKSPACE_ID}': workspace['id'],
-# #     # '{SEMANTIC_MODEL_ID}': model['id'],
-# #     # '{SEMANTIC_MODEL_NAME}': model['displayName'],            
-# # }
-
-# # create_agent_request = \
-# #     ItemDefinitionFactory.update_part_in_create_request(
-# #         create_agent_request,
-# #         'Files/Config/published/semantic-model-Product Sales DirectLake Model/datasource.json',
-# #         agent_redirects)
-
-# # create_agent_request = \
-# #     ItemDefinitionFactory.update_part_in_create_request(
-# #         create_agent_request,
-# #         'Files/Config/draft/semantic-model-Product Sales DirectLake Model/datasource.json',
-# #         agent_redirects)
-
-# return workspace
-
+full_path = f'{folder_path}parameter.yml'
+with open(full_path, 'w', encoding='utf-8') as file:
+    file.write(params)
