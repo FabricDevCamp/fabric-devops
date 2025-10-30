36ca49b905c63378d711287c2ad6a574faeda326
Ted Pattison <ted@tedpattison.net>
2025-10-30 17:16:45 -0400

    updates

--- src/deploy_solution_to_fabric_environment.py
+++ src/deploy_solution_to_fabric_environment.py
@@ -28,29 +28,6 @@ match os.getenv("GIT_INTEGRATION_PROVIDER"):
         # create ADO project and connect project main repo to workspace
         AdoProjectManager.create_project(workspace_name)
         FabricRestApi.connect_workspace_to_ado_repo(workspace, workspace_name)
-
-        # create feature1 workspace
-        FEATURE1_NAME = 'feature1'
-        FEATURE1_WORKSPACE_NAME = F'{workspace_name}-{FEATURE1_NAME}'
-        FEATURE1_WORKSPACE = FabricRestApi.create_workspace(FEATURE1_WORKSPACE_NAME)
-
-        # create feature1 branch and connect to feature1 workspace
-        AdoProjectManager.create_branch(workspace_name, FEATURE1_NAME, 'main')
-        FabricRestApi.connect_workspace_to_ado_repo(FEATURE1_WORKSPACE, workspace_name, FEATURE1_NAME)
-
-        # apply post sync/deploy fixes to feature1 workspace
-        DeploymentManager.apply_post_deploy_fixes(
-            FEATURE1_WORKSPACE_NAME,
-            StagingEnvironments.get_dev_environment(),
-            True
-        )
-        
-        FabricRestApi.commit_workspace_to_git(
-            FEATURE1_WORKSPACE['id'],
-            commit_comment = 'Sync updates from feature workspace to repo after applying fixes')
-
-        AdoProjectManager.create_and_merge_pull_request(workspace_name, FEATURE1_NAME , 'main')
-
         
     case 'GitHub':
         # create GitHub repo and connect to workspace
@@ -58,28 +35,28 @@ match os.getenv("GIT_INTEGRATION_PROVIDER"):
         GitHubRestApi.create_repository(repo_name)
         FabricRestApi.connect_workspace_to_github_repo(workspace, repo_name)
 
-        # create workspace for feature1
-        FEATURE1_NAME = 'feature1'
-        FEATURE1_WORKSPACE_NAME = F'{workspace_name} - {FEATURE1_NAME}'
-        FEATURE1_WORKSPACE = FabricRestApi.create_workspace(FEATURE1_WORKSPACE_NAME)
+        # # create workspace for feature1
+        # FEATURE1_NAME = 'feature1'
+        # FEATURE1_WORKSPACE_NAME = F'{workspace_name} - {FEATURE1_NAME}'
+        # FEATURE1_WORKSPACE = FabricRestApi.create_workspace(FEATURE1_WORKSPACE_NAME)
 
-        # create feature branch and connect to feature workspace
-        GitHubRestApi.create_branch(repo_name, FEATURE1_NAME)
-        FabricRestApi.connect_workspace_to_github_repo(FEATURE1_WORKSPACE, repo_name, FEATURE1_NAME)
+        # # create feature branch and connect to feature workspace
+        # GitHubRestApi.create_branch(repo_name, FEATURE1_NAME)
+        # FabricRestApi.connect_workspace_to_github_repo(FEATURE1_WORKSPACE, repo_name, FEATURE1_NAME)
 
-        # create workspace for feature2
-        FEATURE2_NAME = 'feature2'
-        FEATURE2_WORKSPACE_NAME = F'{workspace_name} - {FEATURE2_NAME}'
-        FEATURE2_WORKSPACE = FabricRestApi.create_workspace(FEATURE2_WORKSPACE_NAME)
+        # # create workspace for feature2
+        # FEATURE2_NAME = 'feature2'
+        # FEATURE2_WORKSPACE_NAME = F'{workspace_name} - {FEATURE2_NAME}'
+        # FEATURE2_WORKSPACE = FabricRestApi.create_workspace(FEATURE2_WORKSPACE_NAME)
 
-        # create feature branch and connect to feature workspace
-        GitHubRestApi.create_branch(repo_name, FEATURE2_NAME)
-        FabricRestApi.connect_workspace_to_github_repo(FEATURE2_WORKSPACE, repo_name, FEATURE2_NAME)
+        # # create feature branch and connect to feature workspace
+        # GitHubRestApi.create_branch(repo_name, FEATURE2_NAME)
+        # FabricRestApi.connect_workspace_to_github_repo(FEATURE2_WORKSPACE, repo_name, FEATURE2_NAME)
 
-        # apply post sync/deploy fixes to feature2 workspace, but not feature1 workspace
-        DeploymentManager.apply_post_sync_fixes(
-            FEATURE2_WORKSPACE_NAME,
-            StagingEnvironments.get_dev_environment(),
-            True)
+        # # apply post sync/deploy fixes to feature2 workspace, but not feature1 workspace
+        # DeploymentManager.apply_post_sync_fixes(
+        #     FEATURE2_WORKSPACE_NAME,
+        #     StagingEnvironments.get_dev_environment(),
+        #     True)
 
 AppLogger.log_job_complete(workspace['id'])
