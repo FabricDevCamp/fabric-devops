31da1d8eb9827bf921816dad8518c790d796c9b3
Ted Pattison <ted@tedpattison.net>
2025-11-15 18:58:34 -0500

    updates

--- templates/WorkflowActions/ADO_TwoStageGitSetup/src/fabric_devops_utils.py
+++ templates/WorkflowActions/ADO_TwoStageGitSetup/src/fabric_devops_utils.py
@@ -38,14 +38,14 @@ class EnvironmentSettings:
     DEPLOYMENT_JOBS = {
         'dev': {
             'name': 'dev',
-            'web_datasource_path': 'https://fabricdevcamp.blob.core.windows.net/sampledata/ProductSales/dev',
+            'web_datasource_path': 'https://fabricdevcamp.blob.core.windows.net/sampledata/ProductSales/dev/',
             'adls_server': 'https://fabricdevcamp.dfs.core.windows.net/',
             'adls_container_name': 'sampledata', 
             'adls_container_path': '/ProductSales/Prod',            
         },
         "prod": {
             'name': 'prod',
-            'web_datasource_path': 'https://fabricdevcamp.blob.core.windows.net/sampledata/ProductSales/prod',
+            'web_datasource_path': 'https://fabricdevcamp.blob.core.windows.net/sampledata/ProductSales/prod/',
             'adls_server': 'https://fabricdevcamp.dfs.core.windows.net/',
             'adls_container_name': 'sampledata',
             'adls_container_path': '/ProductSales/Prod',
