ff6dcfa39a57e703150528d71f16dfebc8edc32e
Ted Pattison <ted@tedpattison.net>
2025-11-20 15:58:24 -0500

    updates

--- templates/ItemDefinitionTemplateFolders/Create Lakehouse Tables.Notebook/notebook-content.py
+++ templates/ItemDefinitionTemplateFolders/Create Lakehouse Tables.Notebook/notebook-content.py
@@ -9,9 +9,7 @@
 # META     "default_lakehouse_name": "{LAKEHOUSE_NAME}",
 # META     "default_lakehouse_workspace_id": "{WORKSPACE_ID}",
 # META     "known_lakehouses": [
-# META     {
-# META       "id": "{LAKEHOUSE_ID}"
-# META     }
+# META      { "id": "{LAKEHOUSE_ID}" }
 # META     ]
 # META   }
 # META   }
