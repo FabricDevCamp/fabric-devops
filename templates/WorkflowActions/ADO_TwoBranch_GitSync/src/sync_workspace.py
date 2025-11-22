ff618aa94a87875964c4a90c07b2996a383e58fe
Ted Pattison <ted@tedpattison.net>
2025-11-22 10:17:46 -0500

    updates

--- templates/WorkflowActions/ADO_TwoBranch_GitSync/src/sync_workspace.py
+++ templates/WorkflowActions/ADO_TwoBranch_GitSync/src/sync_workspace.py
@@ -12,12 +12,12 @@ AppLogger.log_step(f'Pipeline triggered by PR completing on branch [{branch_name
 
 match branch_name:
     
-    case 'dev':
-        workspace_id = EnvironmentSettings.WORKSPACE_ID_DEV
-        FabricRestApi.update_workspace_from_git(workspace_id)
-        deployment_job = EnvironmentSettings.DEPLOYMENT_JOBS['dev']   
-        DeploymentManager.apply_post_sync_fixes(workspace_id, deployment_job)
-        AppLogger.log_job_complete(workspace_id)
+    # case 'dev':
+    #     workspace_id = EnvironmentSettings.WORKSPACE_ID_DEV
+    #     FabricRestApi.update_workspace_from_git(workspace_id)
+    #     deployment_job = EnvironmentSettings.DEPLOYMENT_JOBS['dev']   
+    #     DeploymentManager.apply_post_sync_fixes(workspace_id, deployment_job)
+    #     AppLogger.log_job_complete(workspace_id)
 
     case 'main':
         workspace_id = EnvironmentSettings.WORKSPACE_ID_PROD
