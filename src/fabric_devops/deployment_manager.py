8616537d8bbdea65652f61673e75fb62f28076b5
Ted Pattison <ted@tedpattison.net>
2026-01-22 15:53:57 -0500

    updates

--- src/fabric_devops/deployment_manager.py
+++ src/fabric_devops/deployment_manager.py
@@ -170,7 +170,7 @@ class DeploymentManager:
 
         create_notebook_request = \
             ItemDefinitionFactory.get_create_item_request_from_folder(
-                'Create Lakehouse Tables')
+                'Create Lakehouse Tables.Report')
 
         notebook_redirects = {
             '{WORKSPACE_ID}': workspace['id'],
