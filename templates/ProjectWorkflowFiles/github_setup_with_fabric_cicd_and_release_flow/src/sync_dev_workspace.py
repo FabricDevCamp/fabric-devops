83ca1ac7ec3299d24433f78886b60e014e98d010
Ted Pattison <ted@tedpattison.net>
2026-03-20 07:16:05 -0400

    updates

--- templates/ProjectWorkflowFiles/github_setup_with_fabric_cicd_and_release_flow/src/sync_dev_workspace.py
+++ templates/ProjectWorkflowFiles/github_setup_with_fabric_cicd_and_release_flow/src/sync_dev_workspace.py
@@ -8,13 +8,13 @@ AppLogger.log_job(
     "Synching workspace by updating from GIT and applying any post-sync fixes..."
 )
 
-branch_name = os.environ.get('BUILD_SOURCEBRANCH').replace('refs/heads/', '')
+branch_name =  os.environ.get('BRANCH_NAME')
 
 AppLogger.log_step(f'Pipeline triggered by pull request completion on branch [{branch_name}]')
 
 match branch_name:
     
-    case 'main':
+    case 'dev':
         workspace_id = EnvironmentSettings.WORKSPACE_ID_DEV
         FabricRestApi.update_workspace_from_git(workspace_id)
         deployment_job = EnvironmentSettings.DEPLOYMENT_JOBS['dev']
