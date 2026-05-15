e1af5b97e968a2445b17b96730a0232ab160e9e8
Ted Pattison <ted@tedpattison.net>
2026-05-15 12:11:12 -0400

    updates

--- src/fabric_devops/environment_settings.py
+++ src/fabric_devops/environment_settings.py
@@ -9,6 +9,8 @@ class EnvironmentSettings:
     AZURE_CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
     AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID")
     SERVICE_PRINCIPAL_OBJECT_ID = os.getenv('SERVICE_PRINCIPAL_OBJECT_ID')
+    
+    AZURE_STORAGE_SAS_TOKEN = os.getenv('AZURE_STORAGE_SAS_TOKEN')
 
     FABRIC_CAPACITY_ID = os.getenv("FABRIC_CAPACITY_ID")
     ADMIN_USER_ID = os.getenv("ADMIN_USER_ID")
@@ -35,7 +37,3 @@ class EnvironmentSettings:
     PERSONAL_ACCESS_TOKEN_GITHUB = os.getenv('PERSONAL_ACCESS_TOKEN_GITHUB')
 
     FABRIC_NONTRIAL_CAPACITY_ID = os.getenv('FABRIC_NONTRIAL_CAPACITY_ID')
-
-    # this read-only SaS token is provided so you can tested out creating ADLS Gen2 connections
-    AZURE_STORAGE_SAS_TOKEN = \
-        r'sv=2024-11-04&ss=b&srt=co&sp=rl&se=2027-05-28T22:50:19Z&st=2025-05-19T14:50:19Z&spr=https&sig=%2FCsr%2F07zsA8EbanP5N1Dy4DAdbKSf7B63iJb1da8LC4%3D'
