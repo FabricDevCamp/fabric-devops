be7da0f54046a63200603142fc1ef6db1a05a90f
Ted Pattison <ted@tedpattison.net>
2026-05-23 12:12:02 -0400

    updates

--- src/test01.py
+++ src/test01.py
@@ -2,6 +2,6 @@
 
 from fabric_devops import GitHubRestApi,  AppLogger
 
-PROJECT_NAME = 'charlie'
+PROJECT_NAME = 'jimmy'
 project = GitHubRestApi.create_repository(PROJECT_NAME)
 
---
+++ src/test02.py
@@ -0,0 +1,21 @@
+"""Setup project with fabric-cicd and release flow """
+import os
+
+from fabric_devops import DeploymentManager,FabricRestApi, AppLogger
+
+
+SOURCE_WORKSPACE_NAME = os.getenv("SOURCE_WORKSPACE")
+TARGET_WORKSPACE_NAME = os.getenv("TARGET_WORKSPACE")
+
+SOURCE_WORKSPACE_NAME = "Customer-Sales"
+TARGET_WORKSPACE_NAME = "Lenny"
+
+source_workspace = FabricRestApi.get_workspace_by_name(SOURCE_WORKSPACE_NAME)
+
+AppLogger.log_step(f"Source workspace: {source_workspace.display_name} [{source_workspace.id}]") 
+
+target_workspace = FabricRestApi.create_workspace(TARGET_WORKSPACE_NAME)
+
+AppLogger.log_step(f"Target workspace: {target_workspace.display_name} [{target_workspace.id}]")
+
+AppLogger.log_job_complete()
